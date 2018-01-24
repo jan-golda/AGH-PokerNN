664a6f4a6a607a1a42dd82b822dd859c080a345d
regzand <regzand@gmail.com>
2018-01-24 21:32:10 +0100

    Changed code structure

--- src/NeuralNetwork.hs
+++ src/NeuralNetwork.hs
@@ -15,9 +15,18 @@ module NeuralNetwork (NeuralNetwork, Layer, networkOutput) where
   layerOutput :: Layer -> Matrix Double -> Matrix Double
   networkOutput :: NeuralNetwork -> Matrix Double -> Matrix Double
 
-  sigmoid :: Double -> Double
-
   weightedInput :: Layer -> Matrix Double -> Matrix Double
+
+  sigmoid :: Double -> Double
+  sigmoid' :: Double -> Double
+
+  layerError :: Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
+  lastLayerError :: Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
+
+  costDerivativeWithRespectToBiases :: Matrix Double -> Matrix Double
+  costDerivativeWithRespectToWeights :: Matrix Double -> Matrix Double -> Matrix Double
+
+  learn :: Matrix Double -> Matrix Double -> Double -> NeuralNetwork -> NeuralNetwork
 
   ---------------------------------------------------------------------------------
   -- IMPLEMENTATION
@@ -26,7 +35,46 @@ module NeuralNetwork (NeuralNetwork, Layer, networkOutput) where
 
   networkOutput []     input = input
   networkOutput (l:ls) input = networkOutput ls (layerOutput l input)
-  
-  sigmoid x = 1/(1 + exp (-x))
 
   weightedInput layer input = Matrix.elementwise (+) (Matrix.multStd (weights layer) input) (biases layer)
+
+  sigmoid x = 1/(1 + exp (-x))
+  sigmoid' x = exp(x) / (1+exp(x))^2
+
+  layerError weightedInput nextWeights nextError =
+      Matrix.elementwise
+        (\a b -> a * sigmoid'(b))
+        (multStd (Matrix.transpose nextWeights) nextError)
+        weightedInput
+
+  lastLayerError weightedInput output expected =
+      Matrix.elementwise
+        (\a b -> a * sigmoid'(b))
+        (Matrix.elementwise (\a b -> b-a) output expected)
+        weightedInput
+
+  costDerivativeWithRespectToBiases error = error
+
+  costDerivativeWithRespectToWeights prevOutput error = Matrix.fromList height width values
+        where
+          height = Matrix.nrows error
+          width = Matrix.nrows prevOutput
+          values = [a * b | a <- errorList, b <- prevOutputList]
+              where
+                errorList = Matrix.toList error
+                prevOutputList = Matrix.toList prevOutput
+
+  learn input expected learningRate []                      = []
+
+  learn input expected learningRate (layer:[])              = [ Layer newWeights newBiases ]
+        where
+          newWeights = Matrix.elementwise (+) (weights layer) (Matrix.scaleMatrix learningRate derWeights)
+          newBiases = Matrix.elementwise (+) (biases layer) (Matrix.scaleMatrix learningRate derBiases)
+              where
+                  derWeights = costDerivativeWithRespectToWeights input error
+                  derBiases = costDerivativeWithRespectToBiases error
+                      where
+                        error = lastLayerError (weightedInput layer) (layerOutput layer input) expected
+
+
+  learn input expected learningRate (layer1:layer2:network) = network
