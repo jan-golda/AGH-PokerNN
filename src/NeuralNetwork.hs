ca875d90f9b4f483989dfa0f44e5507ee5b21287
regzand <regzand@gmail.com>
2018-01-24 18:50:41 +0100

    Added NeuralNetwork implemetation

--- src/NeuralNetwork.hs
+++ src/NeuralNetwork.hs
@@ -1,7 +1,32 @@
-module NeuralNetwork (Neuron, Layer) where
+module NeuralNetwork (NeuralNetwork, Layer, networkOutput) where
 
-  data Neuron = Neuron {bias :: Double, weights :: [Double]} deriving (Show)
+  import Data.Matrix as Matrix
 
-  type Layer = [Neuron]
+  ---------------------------------------------------------------------------------
+  -- TYPE DEFINITIONS
+  ---------------------------------------------------------------------------------
+  data Layer = Layer {weights :: Matrix Double, biases :: Matrix Double} deriving (Show)
 
   type NeuralNetwork = [Layer]
+
+  ---------------------------------------------------------------------------------
+  -- DEFINITIONS
+  ---------------------------------------------------------------------------------
+  layerOutput :: Layer -> Matrix Double -> Matrix Double
+  networkOutput :: NeuralNetwork -> Matrix Double -> Matrix Double
+
+  sigmoid :: Double -> Double
+
+  weightedInput :: Layer -> Matrix Double -> Matrix Double
+
+  ---------------------------------------------------------------------------------
+  -- IMPLEMENTATION
+  ---------------------------------------------------------------------------------
+  layerOutput layer input = Matrix.mapCol (\_ x -> sigmoid x) 1 (weightedInput layer input)
+
+  networkOutput []     input = input
+  networkOutput (l:ls) input = networkOutput ls (layerOutput l input)
+  
+  sigmoid x = 1/(1 + exp (-x))
+
+  weightedInput layer input = Matrix.elementwise (+) (Matrix.multStd (weights layer) input) (biases layer)
