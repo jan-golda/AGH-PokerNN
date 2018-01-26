67327cd3c58ad0d6a632bb09d867a80b5b0bc55c
regzand <regzand@gmail.com>
2018-01-26 03:46:17 +0100

    Fixed missing constructor in export from module

--- src/NeuralNetwork.hs
+++ src/NeuralNetwork.hs
@@ -1,4 +1,4 @@
-module NeuralNetwork (NeuralNetwork, Layer, feed, learn) where
+module NeuralNetwork (NeuralNetwork, Layer(Layer), feed, learn) where
 
   import Data.Matrix as Matrix
 
@@ -13,74 +13,74 @@ module NeuralNetwork (NeuralNetwork, Layer, feed, learn) where
   ---------------------------------------------------------------------------------
   -- FUNCTIONS
   ---------------------------------------------------------------------------------
-  
+
   -- Calculates output vector for a single layer
-  
+
   layerOutput :: Layer -> Matrix Double -> Matrix Double
   layerOutput layer input = Matrix.mapCol (\_ x -> sigmoid x) 1 (weightedInput layer input)
-  
 
-  
+
+
   -- Calculates output of neural network by feeding signal forward through each layer
-  
+
   feed :: NeuralNetwork -> Matrix Double -> Matrix Double
   feed []     input = input
   feed (l:ls) input = feed ls (layerOutput l input)
-  
 
-  
+
+
   -- Calculates weighted input for a single layer (sum of all weights * inputs + bias)
-  
+
   weightedInput :: Layer -> Matrix Double -> Matrix Double
   weightedInput layer input = Matrix.elementwise (+) (Matrix.multStd (weights layer) input) (biases layer)
-  
 
-  
+
+
   -- Sigmoid function used for calculating single neuron's output
-  
+
   sigmoid :: Double -> Double
   sigmoid x = 1/(1 + exp (-x))
-  
-  
-  
+
+
+
   -- Sigmoid function derivative
-  
+
   sigmoid' :: Double -> Double
   sigmoid' x = exp(x) / (1+exp(x))^2
-  
-  
+
+
 
   -- Calculates error vector for a single layer (should not be applied to the last layer of a network)
-  
+
   layerError :: Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
   layerError weightedInput nextWeights nextError =
       Matrix.elementwise
         (\a b -> a * sigmoid'(b))
         (multStd (Matrix.transpose nextWeights) nextError)
         weightedInput
-        
-        
+
+
 
   -- Calculates error vector for last layer in network
-  
+
   lastLayerError :: Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
   lastLayerError weightedInput output expected =
       Matrix.elementwise
         (\a b -> a * sigmoid'(b))
         (Matrix.elementwise (\a b -> b-a) output expected)
         weightedInput
-        
-        
+
+
 
   -- Calculates vector of cost function partial derivatives with respect to biases for a single layer
-  
+
   costDerivativeWithRespectToBiases :: Matrix Double -> Matrix Double
   costDerivativeWithRespectToBiases error = error
-  
-  
-  
+
+
+
   -- Calculates vector of cost function partial derivatives with respect to weights for a single layer
-  
+
   costDerivativeWithRespectToWeights :: Matrix Double -> Matrix Double -> Matrix Double
   costDerivativeWithRespectToWeights prevOutput error = Matrix.fromList height width values
         where
@@ -91,22 +91,22 @@ module NeuralNetwork (NeuralNetwork, Layer, feed, learn) where
                 errorList = Matrix.toList error
                 prevOutputList = Matrix.toList prevOutput
 
-                
-                
-  -- Performs network learning on a given dataset with gradient descent cost function minimization 
-  
+
+
+  -- Performs network learning on a given dataset with gradient descent cost function minimization
+
   learn :: Matrix Double -> Matrix Double -> Double -> NeuralNetwork -> NeuralNetwork
   learn input expected learningRate [] = []
 
   learn input expected learningRate network = newNetwork
-    where (newNetwork, _) = backpropagation input expected learningRate network 
-  
-  
-  
+    where (newNetwork, _) = backpropagation input expected learningRate network
+
+
+
   -- Applies backpropagation learning algorithm to neural network
-  
+
   backpropagation :: Matrix Double -> Matrix Double -> Double -> NeuralNetwork -> (NeuralNetwork, Matrix Double)
-  backpropagation input expected learningRate (layer1:layer2:rest) = 
+  backpropagation input expected learningRate (layer1:layer2:rest) =
         let
             (network, nextError) = backpropagation (layerOutput layer1 input) expected learningRate (layer2:rest)
             error = layerError (weightedInput layer1 input) (weights layer2) nextError
@@ -116,9 +116,9 @@ module NeuralNetwork (NeuralNetwork, Layer, feed, learn) where
             newBiases = Matrix.elementwise (+) (biases layer1) (Matrix.scaleMatrix learningRate derBiases)
         in
             ([ Layer newWeights newBiases ] ++ network, error)
-            
-                
-  backpropagation input expected learningRate (layer:[]) = 
+
+
+  backpropagation input expected learningRate (layer:[]) =
         let
             error = lastLayerError (weightedInput layer input) (layerOutput layer input) expected
             derWeights = costDerivativeWithRespectToWeights input error
@@ -128,7 +128,7 @@ module NeuralNetwork (NeuralNetwork, Layer, feed, learn) where
         in
             ([ Layer newWeights newBiases ], error)
 
-            
+
 {-
   -- Little network sample for testing purposes
   ll = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
@@ -142,6 +142,6 @@ module NeuralNetwork (NeuralNetwork, Layer, feed, learn) where
   input = Matrix.fromList 2 1 [0.0, 1]
   expected = Matrix.fromList 2 1 [1.0, 0]
   ratio = 0.4
-  
+
   test = learn input expected ratio
   -}
