a51c0b133f49090823f5b35c9cdc3d2d804e021e
regzand <regzand@gmail.com>
2018-02-08 14:56:47 +0100

    Code cleanup

--- src/PokerNN.hs
+++ src/PokerNN.hs
@@ -1,15 +1,39 @@
+{-
+-- Int list representation of test case input:
+-- "1s,1r,2s,2r,3s,3r,4s,4r,5s,5r"
+-- where:
+--    ks - suit of k-th card (1 - Hearts, 2 - Spades, 3 - Diamonds, 4 - Clubs)
+--    kr - rank of k-th card (1 - Ace, 2 - 2, 3 - 3, ..., 12 - Queen, 13 - King)
+--
+-- Int representation of test case output is a single digit representing hands as follows:
+-- 0: Nothing in hand (high card); not a recognized poker hand
+-- 1: One pair; one pair of equal ranks within five cards
+-- 2: Two pairs; two pairs of equal ranks within five cards
+-- 3: Three of a kind; three equal ranks within five cards
+-- 4: Straight; five cards, sequentially ranked with no gaps
+-- 5: Flush; five cards with the same suit
+-- 6: Full house; pair + different rank three of a kind
+-- 7: Four of a kind; four equal ranks within five cards
+-- 8: Straight flush; straight + flush
+-- 9: Royal flush; {Ace, King, Queen, Jack, Ten} + flush
+--
+-- Matrix representation of input is a single column matrix with 52 rows, each row representing presence (1) or absence (0) of a card in our 5-card hand
+-- First 13 rows represent Hearts, next 13 - Spades, next - Diamonds and finally Clubs
+--
+-- Matrix representation of output is a singla column matrix with 10 rows, all zeros except for row corresponding with proper hand strength
+-}
+
 module PokerNN (stringToTrainingSet, outputToString, stringToInput) where
 
   import NeuralNetwork.Training
 
   import Data.Matrix as Matrix
-
   import Data.Char as Char
 
   ---------------------------------------------------------------------------------
   -- MAIN FUNCTIONS
   ---------------------------------------------------------------------------------
-  
+
   -- | Converts raw String TrainingSet representation to actual TrainingSet
   stringToTrainingSet :: String -> Int -> TrainingSet
   stringToTrainingSet string casesNumber = take casesNumber $ intListToTrainingSet ( map (read :: String -> Int) (words string) )
@@ -75,28 +99,3 @@ module PokerNN (stringToTrainingSet, outputToString, stringToInput) where
   intListToTrainingSet list =
       if length list < 11 then []
                           else [ intListToTrainingCase list ] ++ (intListToTrainingSet (drop 11 list))
-
-  {-
-  -- Int list representation of test case input:
-  -- "1s,1r,2s,2r,3s,3r,4s,4r,5s,5r"
-  -- where:
-  --    ks - suit of k-th card (1 - Hearts, 2 - Spades, 3 - Diamonds, 4 - Clubs)
-  --    kr - rank of k-th card (1 - Ace, 2 - 2, 3 - 3, ..., 12 - Queen, 13 - King)
-  --
-  -- Int representation of test case output is a single digit representing hands as follows:
-  -- 0: Nothing in hand (high card); not a recognized poker hand
-  -- 1: One pair; one pair of equal ranks within five cards
-  -- 2: Two pairs; two pairs of equal ranks within five cards
-  -- 3: Three of a kind; three equal ranks within five cards
-  -- 4: Straight; five cards, sequentially ranked with no gaps
-  -- 5: Flush; five cards with the same suit
-  -- 6: Full house; pair + different rank three of a kind
-  -- 7: Four of a kind; four equal ranks within five cards
-  -- 8: Straight flush; straight + flush
-  -- 9: Royal flush; {Ace, King, Queen, Jack, Ten} + flush
-  --
-  -- Matrix representation of input is a single column matrix with 52 rows, each row representing presence (1) or absence (0) of a card in our 5-card hand
-  -- First 13 rows represent Hearts, next 13 - Spades, next - Diamonds and finally Clubs
-  --
-  -- Matrix representation of output is a singla column matrix with 10 rows, all zeros except for row corresponding with proper hand strength
-  -}
--- test/Spec.hs
+++ test/Spec.hs
@@ -33,46 +33,55 @@ nnTests = TestList $ [
           TestLabel "Feed 3L NN #2" testFeed8,
           TestLabel "Feed 3L NN #3" testFeed9]
 
+-- | Test #1 of calculations on 1 layer NeuralNetwork
 testFeed1 :: Test
 testFeed1 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 2 1 [0.9999999866399913,0.9121360851706988])
       (feed nn1 (Matrix.fromList 3 1 [0.4, 0.9, 0.0]))
 
+-- | Test #2 of calculations on 1 layer NeuralNetwork
 testFeed2 :: Test
 testFeed2 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 2 1 [0.9999999999994043,0.8205384805926733])
       (feed nn1 (Matrix.fromList 3 1 [2.4, -0.9, 0.0]))
 
+-- | Test #3 of calculations on 1 layer NeuralNetwork
 testFeed3 :: Test
 testFeed3 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 2 1 [0.9809228026847419,0.9999451031670289])
       (feed nn1 (Matrix.fromList 3 1 [0.0, 0.0, 1.0]))
 
+-- | Test #1 of calculations on 2 layer NeuralNetwork
 testFeed4 :: Test
 testFeed4 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 3 1 [0.9950331982178893,0.999999997254764,0.9999999997616016])
       (feed nn2 (Matrix.fromList 3 1 [0.4, 0.9, 0.0]))
 
+-- | Test #2 of calculations on 2 layer NeuralNetwork
 testFeed5 :: Test
 testFeed5 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 3 1 [0.9950331983499373,0.9999999958921633,0.9999999996798188])
       (feed nn2 (Matrix.fromList 3 1 [2.4, -0.9, 0.0]))
 
+-- | Test #3 of calculations on 2 layer NeuralNetwork
 testFeed6 :: Test
 testFeed6 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 3 1 [0.994841027931582,0.999999998109462,0.9999999997764508])
       (feed nn2 (Matrix.fromList 3 1 [0.0, 0.0, 1.0]))
 
+-- | Test #1 of calculations on 3 layer NeuralNetwork
 testFeed7 :: Test
 testFeed7 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 1 1 [1.0])
       (feed nn3 (Matrix.fromList 3 1 [0.4, 0.9, 0.0]))
 
+-- | Test #2 of calculations on 3 layer NeuralNetwork
 testFeed8 :: Test
 testFeed8 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 1 1 [1.0])
       (feed nn3 (Matrix.fromList 3 1 [2.4, -0.9, 0.0]))
 
+-- | Test #3 of calculations on 3 layer NeuralNetwork
 testFeed9 :: Test
 testFeed9 = TestCase $ assertEqual "NN wrongly calculated output"
       (Matrix.fromList 1 1 [1.0])
@@ -89,31 +98,37 @@ ioTests = TestList $ [
           TestLabel "Write 2L NN" testToString2,
           TestLabel "Write 3L NN" testToString3]
 
+-- | Test #1 of fromString from NeuralNetwork.IO module
 testFromString1 :: Test
 testFromString1 = TestCase $ assertEqual "NN wrongly readed from string"
       nn1
       (fromString "14.0 9.99 0.4 1.3 1.9 9.7\n3.54 0.11\n")
 
+-- | Test #2 of fromString from NeuralNetwork.IO module
 testFromString2 :: Test
 testFromString2 = TestCase $ assertEqual "NN wrongly readed from string"
       nn2
       (fromString "14.0 9.99 0.4 1.3 1.9 9.7\n3.54 0.11\n2.0 0.0 0.7 4.4 11.45 3.22\n3.3 15.0 7.77\n")
 
+-- | Test #3 of fromString from NeuralNetwork.IO module
 testFromString3 :: Test
 testFromString3 = TestCase $ assertEqual "NN wrongly readed from string"
       nn3
       (fromString "14.0 9.99 0.4 1.3 1.9 9.7\n3.54 0.11\n2.0 0.0 0.7 4.4 11.45 3.22\n3.3 15.0 7.77\n1.1 2.2 3.3\n322\n")
 
+-- | Test #1 of toString from NeuralNetwork.IO module
 testToString1 :: Test
 testToString1 = TestCase $ assertEqual "NN wrongly writed to string"
       "14.0 9.99 0.4 1.3 1.9 9.7\n3.54 0.11\n"
       (toString nn1)
 
+-- | Test #2 of toString from NeuralNetwork.IO module
 testToString2 :: Test
 testToString2 = TestCase $ assertEqual "NN wrongly writed to string"
       "14.0 9.99 0.4 1.3 1.9 9.7\n3.54 0.11\n2.0 0.0 0.7 4.4 11.45 3.22\n3.3 15.0 7.77\n"
       (toString nn2)
 
+-- | Test #3 of toString from NeuralNetwork.IO module
 testToString3 :: Test
 testToString3 = TestCase $ assertEqual "NN wrongly writed to string"
       "14.0 9.99 0.4 1.3 1.9 9.7\n3.54 0.11\n2.0 0.0 0.7 4.4 11.45 3.22\n3.3 15.0 7.77\n1.1 2.2 3.3\n322.0\n"
@@ -130,21 +145,25 @@ ioQuick = do
       putStr "parseFile:\t\t"
       quickCheck quickParseFile
 
+-- | Test of randomLayer from NeuralNetwork.IO module
 quickRandomLayer :: Int -> Int -> Property
 quickRandomLayer nInputs n = monadicIO $ do
       layer <- run $ randomLayer nInputs n
       QCM.assert (checkSize n 1 (biases layer) && checkSize n nInputs (weights layer))
 
+-- | Test of randomNeuralNetwork from NeuralNetwork.IO module
 quickRandomNetwork :: Int -> [Int] -> Property
 quickRandomNetwork nInputs layers = monadicIO $ do
       net <- run $ randomNeuralNetwork nInputs layers
       QCM.assert (length net == length layers)
 
+-- | Test of parseFile and unparseFile from NeuralNetwork.IO module
 quickParseFile :: [[Double]] -> Bool
 quickParseFile list = parseFile (unparseFile list) == list
 
 ---------------------------------------------------------------------------------
 -- UTILIY
 ---------------------------------------------------------------------------------
+-- | Checks if given matrix has the given size
 checkSize :: Int -> Int -> Matrix a -> Bool
 checkSize rows cols m = (nrows m == rows) && (ncols m == cols)
